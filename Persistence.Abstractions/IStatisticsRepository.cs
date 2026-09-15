6ea9115a599dc734eecf72e7fa1b3b93019ba99f
Suha Shehadeh <suha.shehadeh@foothillsolutions.com>
2026-09-15 14:20:55 +0300

    feat: add DB abstraction and MongoDB implementation
    # Conflicts:
    #	MonitoringAndNotificationSystem.sln

---
+++ Persistence.Abstractions/IStatisticsRepository.cs
@@ -0,0 +1,8 @@
+﻿using Shared.Models;
+
+namespace Persistence.Abstractions;
+
+public interface IStatisticsRepository
+{
+    Task SaveAsync(ServerStatistics data);
+}
---
+++ Persistence.MongoDB/MongoDbRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using Persistence.Abstractions;
+using Shared.Models;
+
+namespace Persistence.MongoDB;
+
+public class MongoDbRepository : IStatisticsRepository
+{
+    private readonly IMongoCollection<ServerStatistics> _collection;
+    public MongoDbRepository()
+    {
+        var client = new MongoClient("mongodb://localhost:27017");
+        var database = client.GetDatabase("ServerMonitoringDb");
+        _collection = database.GetCollection<ServerStatistics>("ServerStatistics");
+    }
+
+    public async Task SaveAsync(ServerStatistics data)
+    {
+        Console.WriteLine($"{JsonSerializer.Serialize(data)} Added to mongodb successfully");
+    }
+}
